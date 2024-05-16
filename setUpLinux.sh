5c2b2ef2fe00fa0a80cd314306de9c2830d89320
shivam <shivammadlani5@gmail.com>
2024-05-16 20:37:39 +0530

    replaced apt with brew

--- setUpLinux.sh
+++ setUpLinux.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Check if running as root
-if [ "$(id -u)" != "0" ]; then
-    echo "This script requires superuser privileges. Please run with sudo."
-    exit 1
-fi
-
 # Set script name
 script_name=$(basename "$0")
 script_name="${script_name%.*}"
@@ -36,14 +30,6 @@ log_info() {
     echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: $1" >> "$TMP"
 }
 
-# Updating Package Manager
-log_info "Updating package lists..."
-apt update >> "$TMP" 2>&1
-if [ $? -ne 0 ]; then
-    log_error "Failed to update package lists. Aborting..."
-    exit 1
-fi
-
 # Installing Formulas
 log_info "Installing formulas..."
 # List of formulas to install
@@ -53,14 +39,14 @@ formulas=(
     "nodejs"
     "gedit"
     "nano"
-#    "elasticsearch"
-#    "kibana"
-#    "mongodb"
+    "elasticsearch"
+    "kibana"
+    "mongodb"
     "zookeeper"
-#    "kafka"
+    "kafka"
 )
 for formula in "${formulas[@]}"; do
-    apt install -y "$formula" >> "${FORMULA_LOG_DIR}/${formula}.log" 2>&1
+    brew install "$formula" >> "${FORMULA_LOG_DIR}/${formula}.log" 2>&1
 done
 wait # wait for all formula installation to complete
 log_info "Completed installing all formulas. Check logs to see their status"
@@ -69,21 +55,21 @@ log_info "Completed installing all formulas. Check logs to see their status"
 log_info "Installing casks..."
 # List of casks to install
 casks=(
-#    "sublime-text"
-#    "intellij-idea"
+    "sublime-text"
+    "intellij-idea"
     "code"
     "docker"
     "virtualbox"
-#    "mongodb-compass"
+    "mongodb-compass"
     "vlc"
-#    "spotify"
-#    "whatsapp"
-#    "google-drive"
+    "spotify"
+    "whatsapp"
+    "google-drive"
     "google-chrome-stable"
-#    "microsoft-edge"
+    "microsoft-edge"
 )
 for cask in "${casks[@]}"; do
-    apt install -y "$cask" >> "${CASK_LOG_DIR}/${cask}.log" 2>&1
+    brew install "$cask" >> "${CASK_LOG_DIR}/${cask}.log" 2>&1
 done
 wait # wait for all cask installation to complete
 log_info "Completed installing all casks. Check logs to see their status"
